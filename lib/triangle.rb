2d82e03c2b3d02b748bb6a9aba090419b5561a8c
Maximilian Rex Alba <albam476@gmail.com>
2023-03-04 18:51:38 -0500

    commit tree bc549194d95eaea5c6a61d43c5ec202b5e97992e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :a , :b , :c 
+  def initialize (a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+
+    if greater_than_0 || !triangle_inequality_rule
+      raise TriangleError 
+    else
+      case [@a, @b, @c].uniq.count
+      when 1 
+        :equilateral
+      when 2
+        :isosceles
+      when 3 
+        :scalene
+      end
+    end
+  end
+
+  def greater_than_0
+    ![@a , @b , @c].all?{|num| num.positive?}
+  end
+
+  def triangle_inequality_rule
+    @a + @b > @c && @a + @c > @b && @b + @c > @a
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+
